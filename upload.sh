acd08ba398113ac9b9d92f42cf5ff058e8f4075a
不枝雀 <yunxu1019@qq.com>
2020-04-16 02:30:42 +0800

    修改同步脚本

--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@ efront build
 echo build ok
 mv public qfy
 echo copy to server
-rsync -ru --progress qfy   root@efront.cc:/work/efront/public
-# scp -r qfy root@efront.cc:/work/efront/public/
+rsync -ru --progress qfy   root@efront.cc:/wow
+# scp -r qfy root@efront.cc:/wow
 echo done
