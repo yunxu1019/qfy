1b1581352d8357350670f96208e2ac21457361ed
不枝雀 <yunxu1019@qq.com>
2020-09-26 01:20:22 +0800

    标签价格支持回车输入

--- coms/price.js
+++ coms/price.js
@@ -5,17 +5,21 @@ function main({ data, field }) {
     page.innerHTML = price;
     render(page, {
         btn: button,
+        button,
         input,
         prices: data[field.key],
         value: ['', ''],
         add() {
             var { value } = this;
-            if (!value[0] || !value[1]) {
-                alert("请输入价格！");
-                return;
+            if (!value[0]) {
+                alert("请输入价格！"); return 0;
+            }
+            if (!value[1]) {
+                return 2;
             }
             this.prices.push(value.join('~'));
             this.value = ['', ''];
+            return 0;
         }
     });
     return page;
