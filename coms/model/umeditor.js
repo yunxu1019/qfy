2689dde52daf5594b629d07c970e0da0413cdfc4
不枝雀 <yunxu1019@qq.com>
2023-08-06 09:20:46 +0800

    兼容最新efront

--- coms/model/umeditor.js
+++ coms/model/umeditor.js
@@ -5,4 +5,8 @@ document.head.appendChild(link);
 await script("umeditor/third-party/jquery.min.js", "jQuery")
 await script("umeditor/third-party/template.min.js", "etpl");
 await script("umeditor/umeditor.config.js", "UMEDITOR_CONFIG");
+window.modules = {
+    getScreenPosition,
+    init,
+};
 return script("umeditor/umeditor.js", "UM");
